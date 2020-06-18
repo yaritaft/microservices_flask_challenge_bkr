da5b3c4ddbe499ff51f905125a353721454e1a37
Yari Taft <yari.taft@intive.com>
2020-06-18 01:03:33 -0300

    Docker compose and configs reorganization.

--- entrypoint.sh
+++
@@ -1,3 +0,0 @@
-python manage.py db upgrade
-python manage.py load_initial_data
-uwsgi --thunder-lock --ini uwsgi.ini
--- init_database.sh
+++
@@ -1,7 +0,0 @@
-# CREATE, GENERATE AND APPLY MIGRATIONS TO DEV DB.
-
-python manage.py db init 
-python manage.py db migrate --message 'Initial migration' 
-python manage.py db upgrade 
-python manage.py load_initial_data
-
